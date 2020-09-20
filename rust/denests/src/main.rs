744646bd68b9c90b7b882c55a9def3c6e6555f92
Kevin Amado <kamadorueda@gmail.com>
2020-09-20 17:16:28 -0500

    parse json

--- rust/denests/src/main.rs
+++ rust/denests/src/main.rs
@@ -2,11 +2,13 @@ extern crate clap;
 
 // Standard libraries
 use std::env;
-use std::fs;
+use std::io;
+use std::io::prelude::*;
 use std::path;
 
 // Third party libraries
 use clap::{App, Arg};
+use serde_json;
 use uuid::Uuid;
 
 struct Config {
@@ -27,8 +29,8 @@ fn create_temp_dir() -> path::PathBuf {
 }
 
 fn parse_arguments() -> Config {
-    let matches = App::new("Denests")
-        .about("Receive a json stream on stdin, outputs a json stream on stdout")
+    let cli = App::new("Denests")
+        .about("Receive a json stream on stdin, outputs Singer to stdout")
         .arg(
             Arg::with_name("timestamps_detection")
                 .long("--timestamps-detection")
@@ -37,10 +39,33 @@ fn parse_arguments() -> Config {
         .get_matches();
 
     return Config {
-        timestamps_detection: matches.occurrences_of("timestamps_detection") > 0,
+        timestamps_detection: cli.occurrences_of("timestamps_detection") > 0,
         records_dir: create_temp_dir(),
         schemas_dir: create_temp_dir(),
     };
+}
+
+fn load_json(data: &str) -> Result<serde_json::Value, String> {
+    let _: serde_json::Value = match serde_json::from_str(&data) {
+        Ok(json) => return Ok(json),
+        Err(error) => return Err(format!(
+            "While loading JSON, data: {}, error: {}", data, error,
+        )),
+    };
+}
+
+fn process() -> Result<(), String> {
+    for result in io::stdin().lock().lines() {
+        match result {
+            Ok(line) => {
+                let json = load_json(&line)?;
+                eprintln!("{}", json)
+            },
+            Err(error) => return Err(error.to_string()),
+        }
+    }
+
+    return Ok(());
 }
 
 fn main() {
@@ -49,4 +74,9 @@ fn main() {
     eprintln!("timestamps_detection: {}", config.timestamps_detection);
     eprintln!("records_dir: {}", config.records_dir.display());
     eprintln!("schemas_dir: {}", config.schemas_dir.display());
+
+    match process() {
+        Ok(_) => eprintln!("Successfully processed stream!"),
+        Err(e) => eprintln!("An error ocurred: {}", e),
+    };
 }
